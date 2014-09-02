7d9e8525ed859321ec24632a0ed86dde7f1d8f98
Nick Manning <nicholas.manning@gmail.com>
2014-09-02 10:59:00 -0400

    Fixed issues for running in production with Rails 4

--- Gemfile
+++ Gemfile
@@ -5,8 +5,11 @@ gem 'rails', '3.2.11'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
-
+# gem 'sqlite3'
+gem 'capistrano'
+gem 'capistrano-bundler'
+gem 'capistrano-rails'
+gem 'capistrano-rvm', github: "capistrano/rvm"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ Irinamanning::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -59,7 +59,7 @@ Irinamanning::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
+  #### config.active_support.deprecation = :notify
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
