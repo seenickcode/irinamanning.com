47fa4d659413886706a6e7ee7c0967f4e2e4a7fc
Irina Lapteva <irina@Irinas-MacBook-Air.local>
2014-09-02 23:22:22 -0400

    Finished draft 1.0 of the site

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Irinamanning::Application.routes.draw do
   root :to => 'static#index'
 
   match '/' => 'static#index'
-  match '/projects' => 'static#projects'
+  match '/becoming' => 'static#becoming'
+  match '/betterhalf' => 'static#betterhalf'
+  match '/bfi' => 'static#bfi'
+  match '/element' => 'static#element'
+  match '/felix' => 'static#felix'
+  match '/herman' => 'static#herman'
+  match '/irina-logo' => 'static#irina-logo'
+  match '/lonely-planet' => 'static#lonely-planet'
   match '/peak' => 'static#peak'
+  match '/vega' => 'static#vega'
+  match '/webespy' => 'static#webespy'
 end
